4ecd3fe1d24e5ec7c9b8099374a12eb0bfbc77f4
Kur04ka <vanya.kurochkin.23@mail.ru>
2024-10-07 14:28:17 +0700

    change file structure, worked on dockerfiles

--- backend/migrations/init_tables.sql
+++ backend/migrations/init_tables.sql
@@ -1,7 +1,8 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id UUID NOT NULL DEFAULT gen_random_uuid(),
     name TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CREATE INDEX users_id_idx ON users(id)
 )
